bf2d6ee069a1662e5d52abf2c25dfe95a4f4b4f0
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-05-12 15:47:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function makeid(l) {
-  // write your code here
-	let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	let result = "";
-	for(let i=0;i<l;i++ ){
-		result += chars.charAt(
-			Math.floor(Math.random()*chars.length());
-		)
-	}
-	return result;
+  let chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  let result = "";
+
+  for (let i = 0; i < l; i++) {
+    result += chars.charAt(
+      Math.floor(Math.random() * chars.length)
+    );
+  }
+
+  return result;
 }
 
 // Do not change the code below.
